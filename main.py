07e23dd5e23d853cdf1630759df7d196df65446d
Pedro William <100161312+drowill@users.noreply.github.com>
2025-07-07 15:19:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-import os
 import funcoes
-
 
 #########################################
 ### Agenda de Tarefas e Compromissos  ###
 #########################################
 
-
-opMain = ''
-
-
-
 # Programa Principal
 
+opMain = ''
 while opMain != "0":
     opMain = funcoes.menuPrincipal()
-
     if opMain == '1':
         opTarefa = ''
         while opTarefa != "0":
             opTarefa = funcoes.menuTarefa()
-            print()
             if opTarefa == '1':
                 funcoes.criarTarefa()
             elif opTarefa == '2':
@@ -32,110 +24,39 @@ while opMain != "0":
     elif opMain == "2":
         opHorario = ''
         while opHorario != '0':
-            print()
-            print("############################################")
-            print("#####    Você está no Módulo Horário    ####")
-            print("############################################")
-            print("#####   1 - Criar Horário              #####")
-            print("#####   2 - Exibir dados de Horário    #####")
-            print("#####   3 - Alterar dados de Horário   #####")
-            print("#####   4 - Excluir Horário            #####")
-            print("#####   0 - Voltar ao menu principal   #####")
-            opHorario = input("##### Escolha sua opcao: ")
-            print()
+            opHorario = funcoes.menuHorario()
             if opHorario == '1':
-                print()
-                print("############################################")
-                print("########        Criar Horário       ########")
-                print("############################################")
-                print()
-                horario = input("######## Horário: ")
-                print()
-                print("####### Tarefa cadastrada")
+               funcoes.criarHorario()
             elif opHorario == '2':
-                print()
-                print("############################################")
-                print("########   Exibir dados de Horário  ########")
-                print("############################################")
-                print()
-                horario = input("######## Horário: ")
+                funcoes.exibeHorarios()
             elif opHorario == "3":
-                print()
-                print("############################################")
-                print("########  Alterar dados de Horário  ########")
-                print("############################################")
-                print()
-                horario = input("######## Horário: ")
+                funcoes.alterarHorario()
             elif opHorario == '4':
-                print()
-                print("############################################")
-                print("########       Excluir Horário      ########")
-                print("############################################")
-                print()
-                horario = input("######## Horário: ")
+                funcoes.excluirHorario()
     elif opMain == '3':
         opAgend = ''
         while opAgend != '0':
-            print()
-            print("############################################")
-            print("#####  Você está no Módulo Agendamento  ####")
-            print("############################################")
-            print("#####   1 - Criar Agendamento          #####")
-            print("#####   2 - Exibir dados de Agendamento ####")
-            print("#####   3 - Alterar dados de Agendamento ###")
-            print("#####   4 - Excluir Agendamento        #####")
-            print("#####   0 - Voltar ao menu principal   #####")
-            opAgend = input("##### Escolha sua opcao: ")
-            print()
+            opAgend = funcoes.menuAgend()
             if opAgend == '1':
-                print()
-                print("############################################")
-                print("########      Criar Agendamento     ########")
-                print("############################################")
-                print()
-                print("######## Tarefas:")
-                print("######## Horarios:")
-                agendamento = input("######## Agendamento: ")
-                print()
-                print("####### Tarefa cadastrada")
+                funcoes.criarAgend()
             elif opAgend == '2':
-                print()
-                print("############################################")
-                print("######## Exibir dados de Agendamento #######")
-                print("############################################")
-                print()
-                agendamento = input("######## Agendamento: ")
+                funcoes.exibeAgend()
             elif opAgend == "3":
-                print()
-                print("############################################")
-                print("######## Alterar dados de Agendamento  #####")
-                print("############################################")
-                print()
-                agendamento = input("######## Agendamento: ")
+                funcoes.alterarAgend()
             elif opAgend == '4':
-                print()
-                print("############################################")
-                print("########     Excluir Agendamento    ########")
-                print("############################################")
-                print()
-                agendamento = input("######## Agendamento: ")
+                funcoes.excluirAgend()
     elif opMain == '4':
-        print()
-        print("++++++++++++++++++++++++++++++++++++++++++++")
-        print("++                                        ++")
-        print("++            Módulo Relatório            ++")
-        print("++                                        ++")
-        print("++++++++++++++++++++++++++++++++++++++++++++")
-        input("Tecle <ENTER> para continuar...")
-        print()
+        opRelat = ''
+        while opRelat != "0":
+            opRelat = funcoes.menuRelatorio()
+            if opRelat == '1':
+                funcoes.listaTarefas()
+            elif opRelat == '2':
+                funcoes.listaHorarios()
+            elif opRelat == "3":
+                funcoes.listaAgend()
     elif opMain == '0':
-        print()
-        print("++++++++++++++++++++++++++++++++++++++++++++")
-        print("++                                        ++")
-        print("++           Programa encerrado           ++")
-        print("++                                        ++")
-        print("++++++++++++++++++++++++++++++++++++++++++++")
-        print()
+        funcoes.sair()
     else:
         print()
         print("++++++++++++++++++++++++++++++++++++++++++++")
@@ -144,3 +65,8 @@ while opMain != "0":
         print("++                                        ++")
         print("++++++++++++++++++++++++++++++++++++++++++++")
         print()
+        input("Pressione <ENTER>...")
+
+
+# modulo agendamento
+# crud
